dfe0200dc4a63cc0efc7ef7eb1ef43d9f4dcd441
jkrieger6 <jkrieger11@gmail.com>
2023-05-01 11:44:39 -0700

    added code to get linkedin and git buttons to reroute

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
-const themeBtn = document.getElementById(".theme-btn");
+const themeBtn = document.getElementById("#theme-toggle");
 const body = document.querySelector("body");
+const linkedinBtn = document.getElementById("#linkedinBtn");
+const gitBtn = document.getElementById("#gitBtn");
+
+// Event listener for Linkedin/GitHub button
+linkedinBtn.addEventListener('click', function() {
+    window.location.href("https://www.linkedin.com/in/jacob-krieger-043066b8/");
+});
+
+gitBtn.addEventListener('click', function(){
+    window.open("https://github.com/jkrieger6");
+});
 
 // Event listener for theme button
 themeBtn.addEventListener('click', toggleTheme(){
