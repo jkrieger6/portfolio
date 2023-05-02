ac1e5f98f3a1dd928b77757bfa1744bf9a872d2c
jkrieger6 <jkrieger11@gmail.com>
2023-05-02 15:32:34 -0700

    added functinality to git button

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,20 +5,28 @@ const gitBtn = document.getElementById("#gitBtn");
 
 // Event listener for Linkedin/GitHub button
 linkedinBtn.addEventListener('click', renderLinkedin());
-function renderLinkedin () {
-    window.open("https://www.linkedin.com/in/jacob-krieger-043066b8/");
-}
+
+function renderLinkedin (){
+    linkedinBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = '/';
+      });
+};
 
 gitBtn.addEventListener('click', renderGitHub());
-function renderGitHub(){
-    window.open.href("https://github.com/jkrieger6");
-}
+
+function renderGitHub (){
+    gitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = '/';
+        });
+};
 
 // Event listener for theme button
-themeBtn.addEventListener('click', toggleTheme(){
-    if(document.getAttribute("href") == "assets/css/light.css"){
-        document.setAttribute("href", "assets/css/style.css");
-    } else {
-        body.setAttribute("href", "assets/css/light-theme.css");
-    }
-});
+// themeBtn.addEventListener('click', toggleTheme(){
+//     if(document.getAttribute("href") == "assets/css/light.css"){
+//         document.setAttribute("href", "assets/css/style.css");
+//     } else {
+//         body.setAttribute("href", "assets/css/light-theme.css");
+//     }
+// });
