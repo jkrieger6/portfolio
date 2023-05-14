0011844354a286f8101d3952b67962a67e6f2861
jkrieger6 <jkrieger11@gmail.com>
2023-05-14 13:25:40 -0700

    tried to get theme button to work

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,14 @@
 const themeBtn = document.getElementById("#theme-toggle");
 const body = document.querySelector("body");
-const linkedinBtn = document.getElementById("#linkedinBtn");
-const gitBtn = document.getElementById("#gitBtn");
 
-// Event listener for Linkedin/GitHub button
-linkedinBtn.addEventListener('click', renderLinkedin());
+// Toggle theme
+function toggleTheme() {
+    if (document.getAttribute("href") == "assets/css/light.css") {
+        document.setAttribute("href", "assets/css/style.css");
+    } else {
+        body.setAttribute("href", "assets/css/light-theme.css");
+    }
+}
 
-function renderLinkedin (){
-    linkedinBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.location.href = '/';
-      });
-};
-
-gitBtn.addEventListener('click', renderGitHub());
-
-function renderGitHub (){
-    gitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.location.href = '/';
-        });
-};
-
-// Event listener for theme button
-// themeBtn.addEventListener('click', toggleTheme(){
-//     if(document.getAttribute("href") == "assets/css/light.css"){
-//         document.setAttribute("href", "assets/css/style.css");
-//     } else {
-//         body.setAttribute("href", "assets/css/light-theme.css");
-//     }
-// });
+// Event listener
+themeBtn.addEventListener("click", toggleTheme);
